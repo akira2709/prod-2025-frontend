b4ae938bc1526fd892b7429cbd558dc2b064aba7
akira2709 <145766714+akira2709@users.noreply.github.com>
2025-02-28 21:47:16 +0300

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
+import WorkboxPlugin from "workbox-webpack-plugin"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Добавляем плагин Workbox для генерации service worker
+    config.plugins = [
+      ...(config.plugins || []),
+      new WorkboxPlugin.GenerateSW({
+        clientsClaim: true, // Service worker сразу берет управление над страницей
+        skipWaiting: true, // Обновляет service worker без ожидания закрытия всех вкладок
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/, // Кэширует все HTTPS-запросы
+            handler: 'NetworkFirst', // Сначала пытается получить данные из сети, затем из кэша
+            options: {
+              cacheName: 'offlineCache', // Название кэша
+              expiration: {
+                maxEntries: 20, // Максимальное количество записей в кэше
+              },
+            },
+          },
+        ],
+      }),
+    ];
+
+    return config;
+  },
 };
 
 export default nextConfig;
