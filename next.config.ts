0161fd293651bb2077274eb9e7443be412008004
akira2709 <145766714+akira2709@users.noreply.github.com>
2025-02-28 23:40:04 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import WorkboxPlugin from "workbox-webpack-plugin"
 
 const nextConfig: NextConfig = {
+	output: 'export',
   webpack(config) {
     // Добавляем плагин Workbox для генерации service worker
     config.plugins = [
