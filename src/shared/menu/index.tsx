0ab39d36eca9fcd4f3d5a4f3b2df1022e52cf034
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-03-01 11:34:40 +0300

    Styles for menu

--- src/shared/menu/index.tsx
+++ src/shared/menu/index.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link"
 import styles from "./index.module.css"
 
 export const Menu = () => {
 	return (
 		<nav className={styles.menu}>
-
+            <Link className={styles.link} href="/">Промокоды</Link>
+            <Link className={styles.link} href="/about">QR</Link>
+            <Link className={styles.link} href="/contact">Профиль</Link>
 		</nav>
 	)
 }
