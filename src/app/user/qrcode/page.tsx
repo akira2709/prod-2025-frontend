d2c5d4a83ddcc7ac1b754b9d0b072bf214bb49b4
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-03-01 14:17:31 +0300

    Added qrcode functionality

---
+++ src/app/user/qrcode/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import { useQRCode } from 'next-qrcode';
+import styles from "./qrcode.module.css";
+const QRCodePage = () => {
+    const [qrValue, setQrValue] = useState<string>('');
+    useEffect(() => {
+        const fetchQRCode = async () => {
+            try {
+                const response = await fetch('/api/client/qr');
+                setQrValue(await response.text());
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        fetchQRCode();
+    }, []);
+    const { Canvas } = useQRCode();
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                {qrValue ? (
+                    <Canvas
+                    text={qrValue}
+                    options={{
+                    errorCorrectionLevel: 'M',
+                    margin: 3,
+                    scale: 4,
+                    width: 200,
+                    color: {
+                        dark: '#000',
+                        light: '#fff',
+                    },
+                    }}
+                    />
+                ) : (
+                    <p>Загрузка QR-кода...</p>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default QRCodePage;
