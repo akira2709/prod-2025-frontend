09ed7341a04e0e5b917e0511bf62cd59b0dc365a
akira2709 <145766714+akira2709@users.noreply.github.com>
2025-03-01 22:27:19 +0300

    fix

--- src/app/user/qrcode/page.tsx
+++ src/app/user/qrcode/page.tsx
@@ -4,12 +4,12 @@ import { useQRCode } from "next-qrcode"
 import { useFetch } from "@/shared/api/use-fetch"
 
 const QRCodePage = () => {
+  const { Canvas } = useQRCode()
 	const { data, error, isLoading } = useFetch(["qrcode"], {
 		endpoint: "/api/client/qr"
 	})
 	if (isLoading) return <Loader />
 	if (error) return <span>error</span>
-  const { Canvas } = useQRCode()
   if (data) return
   <Canvas
    	text={data}
