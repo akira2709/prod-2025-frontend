0a2828f1a7be6411316573b8c704914af889d589
akira2709 <145766714+akira2709@users.noreply.github.com>
2025-03-01 12:20:02 +0300

    prettier

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 import { Children } from "@/shared/models/chilren.js";
 import { Menu } from "@/widgets/menu";
 import { usePathname } from "next/navigation";
 const UserLayout = ({ children }: Children) => {
-	const pathName = usePathname();
-	const cantBeShowed = ["/user/enter", "user/register"];
-	const showed = !cantBeShowed.includes(pathName);
-	return (
-		<>
-			{ children }
-			{showed && <Menu />}
-		</>
-	)
-}
+  const pathName = usePathname();
+  const cantBeShowed = ["/user/enter", "user/register"];
+  const showed = !cantBeShowed.includes(pathName);
+  return (
+    <>
+      {children}
+      {showed && <Menu />}
+    </>
+  );
+};
 export default UserLayout;
