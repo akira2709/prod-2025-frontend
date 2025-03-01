17dcf5579521133b629c0342bbd2f25fb3503c7a
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-03-01 13:31:09 +0300

    Added sign-in. Probably will be fixed/changed

--- src/app/user/sign-in/page.tsx
+++ src/app/user/sign-in/page.tsx
@@ -1,24 +1,20 @@
 "use client";
-import styles from "./register.module.css";
+import styles from "./sign-in.module.css";
+import React from "react";
 import { useState } from "react";
-interface Register {
-  name: string;
+interface Enter {
   email: string;
   password: string;
 }
-const register = () => {
-  const [data, setData] = useState<Register>({
-    name: "",
-    email: "",
-    password: "",
-  });
+const Sign   = () => {
+  const [data, setData] = useState<Enter>({ email: "", password: "" });
   const processChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setData((prev) => ({ ...prev, [name]: value }));
   };
   const processSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const response = await fetch("/api/client/auth/sign-up", {
+    const response = await fetch("/api/client/auth/sign-in", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -29,37 +25,29 @@ const register = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <h1 className={styles.zagolovok}>Регистрация</h1>
+        <h1 className={styles.zagolovok}>Вход</h1>
         <form onSubmit={processSubmit} className={styles.form}>
-          <label htmlFor="name" className={styles.inputLabel}>Имя</label>
-            <input
-              type="text"
-              name="name"
-              value={data.name}
-              onChange={processChange}
-              className={styles.inputForm}
-            />
-            <label htmlFor="email" className={styles.inputLabel}>Почта</label>
             <input
               type="email"
               name="email"
               value={data.email}
               onChange={processChange}
               className={styles.inputForm}
+              placeholder="Имя..."
             />
-            <label htmlFor="password" className={styles.inputLabel}>Пароль</label>
             <input
               type="password"
               name="password"
               value={data.password}
+              placeholder="Пароль..."
               onChange={processChange}
               className={styles.inputForm}
             />
-          <button type="submit" className={styles.regBtn}>Зарегистрироваться</button>
+          <button type="submit" className={styles.inBtn} onClick={processSubmit}>Войти</button>
         </form>
       </div>
     </div>
   );
 };
 
-export default register;
+export default Sign;
