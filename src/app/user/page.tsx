56fdd4bcc5e2859150791bdd1bb3859dfa62c7d1
akira2709 <145766714+akira2709@users.noreply.github.com>
2025-03-01 11:39:33 +0300

    menu

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-export default function UserPage() {
-    return (
-        <div>
-            <h1>Добро пожаловать в LoyalT!</h1>
-        </div>
-    );
-}
-=======
 
 
 const UserPage = () => {
@@ -17,4 +8,3 @@ const UserPage = () => {
 	)
 }
 export default UserPage;
->>>>>>> f63bd01ed3929da60370b17d9ae8bcea10e252f2
