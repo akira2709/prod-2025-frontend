4f926ed4ee473a79c8ea4a8970ff1f971ba64d31
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-03-02 20:46:46 +0300

    Lint changes and companies

--- src/app/client/companies/page.tsx
+++ src/app/client/companies/page.tsx
@@ -4,10 +4,15 @@ import { Error } from "@/shared/ui/error"
 import { CompanyInfoContainer } from "@/shared/ui/company-info-container"
 import { useFetch } from "@/shared/api/use-fetch"
 import { Loader } from "@/shared/ui/loader"
-type Company = {
-  companyId: string
-  iconUrl: string
+type Loyalty = {
   title: string
+  target_usages: number
+  n_count: number
+}
+type Company = {
+  picture_url: string
+  name: string
+  loyalties: Loyalty[]
 }
 const CompaniesPage = () => {
   // const { data, error, isLoading } = useFetch<Company[]>(["companies"], {
@@ -16,23 +21,19 @@ const CompaniesPage = () => {
   // if (isLoading) return <Loader />
   // if (error) return <Error text="Не удалось загрузить список компаний" />
   // if (data)
-    return (
-      <div className={styles.container}>
-        <h1 className={styles.title}>Компании</h1>
-        <div className={styles.companyList}>
-          {/* {data.map((company: Company) => (
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Компании</h1>
+      <div className={styles.companyList}>
+        {/* {data.map((id: number, company: Company) => (
             <CompanyInfoContainer
-              key={company.companyId}
-              companyId={company.companyId}
-              url={company.iconUrl}
-              title={company.title}
+              key={id}
+              company={company}
             />
-            
           ))} */}
-          <CompanyInfoContainer companyId="1231" url="13123" title="company" />
-        </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default CompaniesPage
--- src/shared/ui/company-info-container/index.tsx
+++ src/shared/ui/company-info-container/index.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react"
-import styles from "./index.module.css"
-
-type Props = {
-  companyId: string
-  url: string
+import { useState } from "react";
+import styles from "./index.module.css";
+import { Container } from "@/shared/ui/container";
+type Loyalty = {
   title: string
+  target_usages: number
+  n_count: number
+}
+type Props = {
+  picture_url: string
+  name: string
+  loyalties: Loyalty[]
 }
 
 export const CompanyInfoContainer = (props: Props) => {
@@ -17,12 +22,20 @@ export const CompanyInfoContainer = (props: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.baseBlock} onClick={toggleOpen}>
-        <img className={styles.icon} src={props.url} alt={props.title} />
-        <p className={styles.title}>{props.title}</p>
+        <img className={styles.icon} src={props.picture_url} alt={props.name} />
+        <p className={styles.title}>{props.name}</p>
         <div className={`${styles.arrow} ${isOpen ? styles.up : ""}`} />
       </div>
       {isOpen && (
         <div className={styles.dropdown}>
+          {props.loyalties.map((loyalty: Loyalty, index: number) => (
+            <div key={index}>
+              <Container>
+                <h2 className={styles.title}>{loyalty.title}</h2>
+                <p className={styles.desc}>{loyalty.target_usages} из {loyalty.n_count}</p>
+              </Container>
+            </div>
+          ))}
         </div>
       )}
     </div>
