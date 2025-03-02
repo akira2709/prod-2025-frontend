d3682c5421337f3588af70aa0b37e6559ac763f9
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-03-02 14:54:45 +0300

    Added header-like menu;

---
+++ src/app/partner/layout.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Children } from "@/shared/models/chilren.js"
+import { Header } from "@/widgets/header";
+const PartnerLayout = ({ children }: Children) => {
+  return (
+    <>
+      {<Header />}
+      {children}
+    </>
+  )
+}
+export default PartnerLayout
--- src/app/partner/page.tsx
+++ src/app/partner/page.tsx
@@ -3,26 +3,6 @@ import styles from "./index.module.css"
 export default function PartnerPage() {
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <a href="/partner/stats" className={styles.links}>
-            Статистика
-          </a>
-          <a href="/partner/campaigns" className={styles.links}>
-            Кампании
-          </a>
-          <div className={styles.profile_link}>
-            <img
-              src="https://source.unsplash.com/random/40x40"
-              alt="Лого"
-              className={styles.logo}
-            />
-            <a href="/partner/profile" className={styles.links}>
-              Профиль
-            </a>
-          </div>
-        </nav>
-      </header>
       <main className={styles.content}>
         <h1>Добро пожаловать</h1>
         <p>
