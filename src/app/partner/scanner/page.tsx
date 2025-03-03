76148262bfc4f69134221431f542e8e63f8b9be2
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-03-03 11:14:06 +0300

    Added global context for client id

--- src/app/partner/scanner/page.tsx
+++ src/app/partner/scanner/page.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Scanner } from "@/widgets/scanner"
-
+import {useClient} from "@/shared/context";
 const ScannerPage = () => {
   return (
   	<Scanner />
--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -7,6 +7,9 @@ export const Header = () => {
         <Link href="/partner" className={styles.links}>
           Главная
         </Link>
+        <Link href="/scanner" className={styles.links}>
+          Сканировать
+        </Link>
         <Link href="/partner/stats" className={styles.links}>
           Статистика
         </Link>
