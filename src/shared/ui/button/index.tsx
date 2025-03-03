bf6da781e6c70f9c63c505f4787cacd9d6ab1f26
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-03-03 14:44:14 +0300

    Changed POST buttons

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./index.module.css"
-import { useFetch } from "@/shared/api/use-fetch"
+import { Fetch } from "@/shared/api/use-fetch"
+import { toast } from "sonner"
 type Props = {
   client_id: string;
   loyalty_id: string;
@@ -7,20 +8,18 @@ type Props = {
   children: React.ReactNode
 }
 export const Button = ({ client_id, loyalty_id, type, children }: Props) => {
-    const { refetch: process } = useFetch(
-      ["clientLoyaltyArray"],
-      {
-        endpoint: `/partner/scan/${client_id}/${loyalty_id}/${type}`,
-        method: "post"
-      },
-      {
-        refetchOnWindowFocus: false,
-        refetchOnMount: false,
-        refetchInterval: false,
-        staleTime: 5 * 60 * 1000,
-        cacheTime: 5 * 60 * 1000,
-      },
-    )
+    const process = async () => {
+      const { data, error } = await Fetch<{status: string}>({
+          endpoint: `/partner/scan/${client_id}/${loyalty_id}/${type}`,
+          method: "post"
+        }
+      )
+      if (error !== null) {
+        toast.error(error.response.data.detail)
+        return
+      }
+      toast.success("Удалось!")
+    }
   return (
     <button onClick={() => process()} className={styles.button}>
       {children}
